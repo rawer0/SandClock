b6813125ca36afe45c25c00eb00410f6e5b71cf0
rawer <rawer.dev@gmail.com>
2017-11-15 13:21:44 +0800

    添加 Realm 对象

--- app/src/main/java/com/ovwvwvo/sandclock/model/SandClockModel.java
+++ app/src/main/java/com/ovwvwvo/sandclock/model/SandClockModel.java
@@ -1,8 +1,67 @@
 package com.ovwvwvo.sandclock.model;
 
+import io.realm.RealmObject;
+import io.realm.annotations.Ignore;
+import io.realm.annotations.Required;
+
 /**
  * Created by guang on 2017/11/8.
  */
 
-public class SandClockModel {
+public class SandClockModel extends RealmObject {
+
+    private long id;//在 Realm 中基本类型不允许为 null
+    @Required
+    private String content;
+    private long targetDate;
+
+    private Integer skin = 0;//可以为 null
+    private int unit = Unit.DALY.ordinal();//单位(年、月、天、小时、分、秒) 可以为 null
+
+    @Ignore
+    private long interval;//间隔
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(long targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public Integer getSkin() {
+        return skin;
+    }
+
+    public void setSkin(Integer skin) {
+        this.skin = skin;
+    }
+
+    public long getInterval() {
+        return interval;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
 }
---
+++ app/src/main/java/com/ovwvwvo/sandclock/model/Unit.java
@@ -0,0 +1,12 @@
+package com.ovwvwvo.sandclock.model;
+
+/**
+ * Created by guang on 2017/11/15.
+ */
+
+public enum Unit {
+    DALY(0), YEAR(1), MONTH(2), HOUR(3);
+
+    Unit(int i) {
+    }
+}
